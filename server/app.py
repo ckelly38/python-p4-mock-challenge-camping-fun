ef2f637ca4037b8411305041d3b0c1ec65ff0764
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2024-01-27 03:00:03 -0700

    all tests pass, need to study foreign keys, relationships, and cascades for said relationships, and serilization

--- server/app.py
+++ server/app.py
@@ -18,12 +18,102 @@ app.json.compact = False
 
 migrate = Migrate(app, db)
 
-db.init_app(app)
+api = Api(app);
 
+db.init_app(app)
 
 @app.route('/')
 def home():
     return ''
+
+class Campers(Resource):
+    def get(self):
+        cmprs = [cmpr.to_dict(rules=("-signups",)) for cmpr in Camper.query.all()];
+        return cmprs, 200;
+
+    def post(self):
+        #print("INSIDE ADD NEW CAMPER-POST():");
+        rjsn = request.get_json();
+        #rdta = request.get_data();
+        #rfrm = request.form;
+        #print(rjsn);
+        #print(rdta);
+        #print(rfrm);
+        ncmpr = None;
+        try:
+            ncmpr = Camper(name=rjsn["name"], age=rjsn["age"]);
+        except Exception as ex:
+            #print("400 ERROR: INVALID CAMPER!");
+            return {"errors": ["validation errors"]}, 400;
+        #add it to the database now
+        db.session.add(ncmpr);
+        db.session.commit();
+        return ncmpr.to_dict(), 201;
+
+api.add_resource(Campers, "/campers");
+
+class CamperByID(Resource):
+    def get(self, id):
+        cmpr = Camper.query.filter_by(id=id).first();
+        if (cmpr == None): return {"error": "Camper not found"}, 404;
+        else: return cmpr.to_dict(), 200;
+
+    def patch(self, id):
+        cmpr = Camper.query.filter_by(id=id).first();
+        if (cmpr == None): return {"error": "Camper not found"}, 404;
+        rjsn = request.get_json();
+        #print(rjsn);
+        try:
+            for attr in rjsn:
+                setattr(cmpr, attr, rjsn[attr]);
+        except Exception as ex:
+            #print("400 ERROR: INVALID CAMPER!");
+            return {"errors": ["validation errors"]}, 400;
+        return cmpr.to_dict(), 202;
+        #print("404 ERROR: NOT DONE WITH UPDATING A CAMPER!");
+        #return {"error": "404 ERROR: NOT DONE YET WITH UPDATING A CAMPER!"}, 404;
+
+api.add_resource(CamperByID, "/campers/<int:id>");
+
+class Activities(Resource):
+    def get(self):
+        acts = [act.to_dict() for act in Activity.query.all()];
+        return acts, 200;
+
+api.add_resource(Activities, "/activities");
+
+class ActivityByID(Resource):
+    def delete(self, id):
+        act = Activity.query.filter_by(id=id).first();
+        if (act == None): return {"error": "Activity not found"}, 404;
+        #remove it from the database
+        db.session.delete(act);
+        db.session.commit();
+        return {}, 204;
+        #print("404 ERROR: NOT DONE WITH DELETING ACTIVITIES!");
+        #return {"error": "404 ERROR: NOT DONE YET WITH DELETING ACTIVITIES!"}, 404;
+
+api.add_resource(ActivityByID, "/activities/<int:id>");
+
+class SignUps(Resource):
+    def post(self):
+        rjsn = request.get_json();
+        print(rjsn);
+        snup = None;
+        try:
+            snup = Signup(activity_id=rjsn["activity_id"], camper_id=rjsn["camper_id"],
+                          time=rjsn["time"]);
+        except Exception as ex:
+            #print("400 ERROR: INVALID SIGNUP!");
+            return {"errors": ["validation errors"]}, 400;
+        #add it to the database
+        db.session.add(snup);
+        db.session.commit();
+        return snup.to_dict(), 200;
+        #print("404 ERROR: NOT DONE WITH MAKING SIGNUPS!");
+        #return {"error": "404 ERROR: NOT DONE YET WITH MAKING SIGNUPS!"}, 404;
+
+api.add_resource(SignUps, "/signups");
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -16,6 +16,62 @@ metadata = MetaData(naming_convention=convention)
 
 db = SQLAlchemy(metadata=metadata)
 
+class MyValidator:
+    def __init__(self):
+        self.disablevalidator = False;
+
+    def strValHasAtMinXChars(self, val, x):
+        if (type(x) == int): pass;
+        else: raise ValueError("x must be an integer!");
+        if (x < 0): raise ValueError("x must not be less than zero!");
+        if (self.disablevalidator): return True;
+        if (x < 1):
+            if (val == None): return True;
+            elif (type(val) == str): return True;
+            else: raise ValueError("val must be a string!");
+        else:
+            if (val == None): return False;
+            elif (type(val) == str): return (not(len(val) < x));
+            else: raise ValueError("val must be a string!");
+
+    def intValIsAtMaxOrAtMinX(self, val, x, usemax):
+        if (usemax == True or usemax == False): pass;
+        else: raise ValueError("usemax must be a boolean!");
+        if (val == None or x == None): raise ValueError("x and val must be numbers!");
+        if (type(val) == int and type(x) == int): pass;
+        else: raise ValueError("x and val must be numbers!");
+        if (self.disablevalidator): return True;
+        #usemax means val <= x mean val must not be > x
+        #usemin = true means x<=val means must not be val < x
+        if (usemax): return (not(x < val));
+        else: return (not(val < x));
+
+    def intValIsAtMinimumX(self, val, x):
+        return self.intValIsAtMaxOrAtMinX(val, x, False);
+
+    def intValIsAtMaximumX(self, val, x):
+        return self.intValIsAtMaxOrAtMinX(val, x, True);
+
+    def intValIsAtMinXAndAtMaxY(self, val, x, y, varname = "varname"):
+        #print(f"val = {val}");
+        #print(f"x = {x}");
+        #print(f"y = {y}");
+        #print(f"varname = {varname}");
+        vnmvld = self.strValHasAtMinXChars(varname, 1);
+        if (vnmvld): pass;
+        else: raise ValueError("varname must have at minimum 1 character on it!");
+        if (y == None or x == None): raise ValueError("x and y must be numbers!");
+        if (type(y) == int and type(x) == int): pass;
+        else: raise ValueError("x and y must be numbers!");
+        ageminvld = mv.intValIsAtMinimumX(val, x);
+        if (ageminvld): pass;
+        else: raise ValueError(f"{varname} must be at minimum {x}!");
+        agemaxvld = mv.intValIsAtMaximumX(val, y);
+        if (agemaxvld): pass;
+        else: raise ValueError(f"{varname} must be at maximum {y}!");
+        return val;
+
+mv = MyValidator();
 
 class Activity(db.Model, SerializerMixin):
     __tablename__ = 'activities'
@@ -25,8 +81,11 @@ class Activity(db.Model, SerializerMixin):
     difficulty = db.Column(db.Integer)
 
     # Add relationship
+    signups = db.relationship("Signup", back_populates="activity", cascade="all, delete-orphan");
     
     # Add serialization rules
+    #serialize_rules = ("-signups",);
+    serialize_rules = ("-signups.activity",);
     
     def __repr__(self):
         return f'<Activity {self.id}: {self.name}>'
@@ -40,10 +99,22 @@ class Camper(db.Model, SerializerMixin):
     age = db.Column(db.Integer)
 
     # Add relationship
+    signups = db.relationship("Signup", back_populates="camper", cascade="all, delete-orphan");
     
     # Add serialization rules
+    #serialize_rules = ("-signups",);
+    serialize_rules = ("-signups.camper",);
     
     # Add validation
+    @validates("name")
+    def isvalidname(self, key, val):
+        nmvld = mv.strValHasAtMinXChars(val, 1);
+        if (nmvld): return val;
+        else: raise ValueError("the camper must have a name!");
+
+    @validates("age")
+    def isvalidage(self, key, val):
+        return mv.intValIsAtMinXAndAtMaxY(val, 8, 18, "age");
     
     
     def __repr__(self):
@@ -57,10 +128,19 @@ class Signup(db.Model, SerializerMixin):
     time = db.Column(db.Integer)
 
     # Add relationships
+    camper_id = db.Column(db.Integer, db.ForeignKey("campers.id"));
+    activity_id = db.Column(db.Integer, db.ForeignKey("activities.id"));
+    camper = db.relationship("Camper", back_populates="signups");
+    activity = db.relationship("Activity", back_populates="signups");
     
     # Add serialization rules
+    #serialize_rules = ("-camper", "-activity");
+    serialize_rules = ("-campers.signups", "-activity.signups");
     
     # Add validation
+    @validates("time")
+    def isvalidtime(self, key, val):
+        return mv.intValIsAtMinXAndAtMaxY(val, 0, 23, "time");
     
     def __repr__(self):
         return f'<Signup {self.id}>'
